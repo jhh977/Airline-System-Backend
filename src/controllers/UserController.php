59b1b4ea7b95844974ef0df716dcb102194e0b70
Majd <majdbitar2003@gmail.com>
2024-07-17 14:40:26 +0300

    worked on reg

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -17,7 +17,7 @@ class UserController
     public function register()
     {
         $data = json_decode(file_get_contents('php://input'), true);
-        $data = $_POST;
+        //$data = $_POST;
         $name = $data['name'] ;
         $email = $data['email'] ;
         $password = $data['password'] ;
